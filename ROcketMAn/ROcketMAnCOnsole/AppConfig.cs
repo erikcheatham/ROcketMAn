c485084b910b971e8f49ab1f848437ea0bbc9cec
Erik Cheatham <erikcheatham@gmail.com>
2018-01-08 13:50:05 -0600

    Changed To NetCore Project & Initial Startup

---
+++ ROcketMAn/ROcketMAnCOnsole/AppConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROcketMAnCOnsole
+{
+    public static class AppConfig
+    {
+        public static string DiscordClientID;
+        public static string DiscordAPIKey;
+        public static string BaseServiceUrl;
+        public static ROcketMAnEnvironment environment;
+        //public static UserType MockedRole;
+        //public static UserType UserRole;
+
+        public static void InitEnv(ROcketMAnEnvironment env)
+        {
+            environment = env;
+            switch (env)
+            {
+                case ROcketMAnEnvironment.Mocked:
+                    {
+                        DiscordClientID = "399970090084335627";
+                        DiscordAPIKey = "Mzk5OTcwMDkwMDg0MzM1NjI3.DTVL7w.OGcA_opEPC14yNJkU5k4Mk0aYF4";
+                        BaseServiceUrl = $"https://discordapp.com/oauth2/authorize?client_id={DiscordClientID}&scope=bot";
+                        break;
+                    }
+                case ROcketMAnEnvironment.Dev:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        public enum ROcketMAnEnvironment
+        {
+            Mocked,
+            Dev
+        }
+    }
+}
---
+++ ROcketMAn/ROcketMAnCOnsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace ROcketMAnCOnsole
+{
+    public class Program
+    {
+        private CommandService cmd;
+        private DiscordSocketClient cl;
+        private IServiceProvider serv;
+        private ConfigurationSection ccs;
+
+        public static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+        
+        public async Task Start()
+        {
+            var env = ROcketMAnCOnsole.AppConfig.ROcketMAnEnvironment.Mocked;
+            //var env = ROcketMAnCOnsole.AppConfig.ROcketMAnEnvironment.Dev;
+            AppConfig.InitEnv(env);
+
+            //throw new NotImplementedException();
+            cl = new DiscordSocketClient();
+            cmd = new CommandService();
+
+            cl.Log += Log;
+
+            await cl.LoginAsync(TokenType.Bot, AppConfig.DiscordAPIKey);
+            await cl.StartAsync();
+
+            //Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        //private async Task Mess
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
