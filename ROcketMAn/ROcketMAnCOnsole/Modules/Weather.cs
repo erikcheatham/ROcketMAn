bb82071cce0192e063b783fc8715bf177c121a83
Erik Cheatham <erikcheatham@gmail.com>
2018-01-12 17:29:34 -0600

    Push One

---
+++ ROcketMAn/ROcketMAnCOnsole/Modules/Weather.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace ROcketMAnCOnsole.Modules
+{
+    public class Weather : ModuleBase<SocketCommandContext>
+    {
+        public async Task SayMyWeather([Remainder] [Summary("The text to echo")] string echo)
+        {
+            await ReplyAsync(echo);
+        }
+    }
+
+    [Group("sample")]
+    public class Sample : ModuleBase<SocketCommandContext>
+    {
+        // ~sample square 20 -> 400
+        [Command("square")]
+        [Summary("Squares a number.")]
+        public async Task SquareAsync([Summary("The number to square.")] int num)
+        {
+            // We can also access the channel from the Command Context.
+            await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+        }
+
+        // ~sample userinfo --> foxbot#0282
+        // ~sample userinfo @Khionu --> Khionu#8708
+        // ~sample userinfo Khionu#8708 --> Khionu#8708
+        // ~sample userinfo Khionu --> Khionu#8708
+        // ~sample userinfo 96642168176807936 --> Khionu#8708
+        // ~sample whois 96642168176807936 --> Khionu#8708
+        [Command("userinfo")]
+        [Summary("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync([Summary("The (optional) user to get info for")] SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+    }
+
+    [Group("admin")]
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Group("clean")]
+        public class CleanModule : ModuleBase<SocketCommandContext>
+        {
+            // ~admin clean 15
+            [Command]
+            public async Task Default(int count = 10) => Messages(count);
+
+            // ~admin clean messages 15
+            [Command("messages")]
+            public void Messages(int count = 10) { }
+        }
+        // ~admin ban foxbot#0282
+        [Command("ban")]
+        public void Ban(IGuildUser user) { }
+    }
+
+}
--- ROcketMAn/ROcketMAnCOnsole/Program.cs
+++ ROcketMAn/ROcketMAnCOnsole/Program.cs
@@ -8,6 +8,8 @@ using Discord.WebSocket;
 using Discord.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using ROcketMAnCOnsole.Modules;
+using System.Collections.Generic;
 
 namespace ROcketMAnCOnsole
 {
@@ -22,18 +24,26 @@ namespace ROcketMAnCOnsole
         //{
         //    Console.WriteLine("Hello World!");
         //}
-        
+
         public async Task Start()
         {
+            //throw new NotImplementedException();
+
             var env = ROcketMAnCOnsole.AppConfig.ROcketMAnEnvironment.Mocked;
             //var env = ROcketMAnCOnsole.AppConfig.ROcketMAnEnvironment.Dev;
             AppConfig.InitEnv(env);
 
-            //throw new NotImplementedException();
             cl = new DiscordSocketClient();
             cmd = new CommandService();
 
-            cl.Log += Log;
+            //cl.Log += Log;
+
+            serv = new ServiceCollection()
+                .AddSingleton(cl)
+                .AddSingleton(cmd)
+                .BuildServiceProvider();
+
+            await InstallCommandsAsync();
 
             await cl.LoginAsync(TokenType.Bot, AppConfig.DiscordAPIKey);
             await cl.StartAsync();
@@ -42,7 +52,106 @@ namespace ROcketMAnCOnsole
             await Task.Delay(-1);
         }
 
-        //private async Task Mess
+        //public async Task InstallAsync()
+        //{
+        //    cl.Log += Log;
+        //    // Hook the MessageReceived Event into our Command Handler
+        //    cl.MessageReceived += HandleCommandAsync;
+        //    // Here, we will inject the ServiceProvider with 
+        //    // all of the services our client will use.
+        //    serv = new ServiceCollection()
+        //        .AddSingleton(cl)
+        //        .AddSingleton(cmd)
+        //        // You can pass in an instance of the desired type
+        //        .AddSingleton(new NotificationService())
+        //        // ...or by using the generic method.
+        //        //.AddSingleton<DatabaseService>()
+        //        .BuildServiceProvider();
+        //    // ...
+        //    await cmd.AddModulesAsync(Assembly.GetEntryAssembly());
+        //}
+
+
+        private async Task InstallCommandsAsync()
+        {
+            cl.Log += Log;
+            // Hook the MessageReceived Event into our Command Handler
+            cl.MessageReceived += HandleCommandAsync;
+            // Discover all of the commands in this assembly and load them.
+            await cmd.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasCharPrefix('r', ref argPos) || message.HasMentionPrefix(cl.CurrentUser, ref argPos))) return;
+            {
+                //if (message.Content.ToString() == "rha")
+                //{
+                //    //Weather wc = new Weather();
+                //    //await wc.SayMyWeather("weather bitch!");
+                //    // Create a Command Context
+                var context = new SocketCommandContext(cl, message);
+                //    //var context = new SocketCommandContext(cl, message);
+
+                //    //SocketUserMessage bot = new SocketUserMessage();
+
+                //    message.MentionedUsers = "IRishShark";
+
+
+                //    bot.MentionedUsers = "IRishShark";
+
+
+                //var context = new SocketCommandContext(cl, message =>
+                //{
+                //    IMessage = "@IRishShark you a sucka g";
+                //        //SocketUser user = user.Username = "";
+                //    });
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed successfully)
+                var result = await cmd.ExecuteAsync(context, argPos, serv);
+                if (!result.IsSuccess)
+                {
+                    var currentUser = context.Client.CurrentUser;
+                    //List<SocketGuildUser> us = new List<SocketGuildUser>();
+                    //ulong g = 0;
+                    //if(ulong.TryParse(AppConfig.DiscordClientID, out g))
+                    //{
+                    //    var users = context.Client.DownloadUsersAsync(g).toList();
+                    //}
+
+                    var guild = cl.GetGuild(ulong.Parse(AppConfig.DiscordClientID));
+
+                    //var users = context.Client.UserJoined().Id = "";
+
+                    //var users = await context.User
+
+
+                    var guilds = context.Client.Guilds;
+                    var us = await context.Client.DownloadUsersAsync(guilds);
+
+                    if (context.User.Username == "IrishShark")
+                    //context.User.Username = "IrishShark";
+                    //context.Guild.GetUser = "Irishshark";
+                    //context.Message = "@IRishShark you a sucka g";
+                    foreach (var u in context.Client.DownloadUsersAsync)
+                    context.User.Username = "IrishShark";
+                    //var u = context.Message.MentionedUsers = { "Irish" };
+                    var user = context.Client.GetUser("IrishShark", "@");
+                    //foreach (var user in users)
+
+                    await context.Channel.SendMessageAsync(user, "you a sucka g");
+                }
+                //await context.Channel.SendMessageAsync(result.ErrorReason);
+                //else
+                //    await context.Channel.SendMessageAsync("weather bitch!");
+            }
+        }
 
         private Task Log(LogMessage msg)
         {
